3df05bbdc28f2308e15d1018fbde0dd9e32f5db8
ewertondrigues02 <ewertonrodrigues01@gmail.com>
2024-12-16 19:47:30 -0300

    mod15

---
+++ mod15/AbstractFactory/src/br/com/ewertonrodrigues/factory/CorolaCar.java
@@ -0,0 +1,11 @@
+package br.com.ewertonrodrigues.factory;
+
+/**
+ * @author ewertonrodrigues
+ */
+public class CorolaCar extends Car{
+
+    public CorolaCar(int horsePower, String fuelSource, String color) {
+        super(horsePower, fuelSource, color);
+    }
+}
